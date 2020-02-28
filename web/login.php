2f8ce131f42546d049b5bfbbac9c0e806b81dfb4
marcbryan <marcboakyeflores98@gmail.com>
2020-02-28 19:52:03 +0100

    Mostrar errores v2

--- web/login.php
+++ web/login.php
@@ -17,7 +17,7 @@
       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $user = $_POST['username'];
         $pass = $_POST['password'];
-        $query = "SELECT * FROM users WHERE username=$user and password=$password";
+        $query = "SELECT * FROM users WHERE username='$user' and password='$password'";
         $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
         if (pg_num_rowd($result) == 1) {
           echo "<p>Login: OK</p>";
