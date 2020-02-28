6734029e4de4094e8ffb4ab53de3855a98d226fa
marcbryan <marcboakyeflores98@gmail.com>
2020-02-28 19:47:36 +0100

    Primer commit

---
+++ web/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <form action="login.php" method="post">
+      <input type="text" name="username" placeholder="Nombre de usuario">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" value="Iniciar sesión">
+    </form>
+    <?php
+      $db = getenv("DATABASE_URL");
+      $conn = pg_connect($db) or die('Error: '.pg_last_error());
+      echo '<p>Conectado</p>';
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        $query = "SELECT * FROM users WHERE username=$user and password=$password";
+        $result = pg_query($query);
+        if (pg_num_rowd($result) == 1) {
+          echo "<p>Login: OK</p>";
+        } else {
+          echo "<p>Login: Failed</p>";
+        }
+      }
+     ?>
+  </body>
+</html>
